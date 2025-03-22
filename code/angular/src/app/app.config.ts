f14f1c89817ecc01777c4dc3f31b234ac537e59c
Gareth Erskine-Jones <gsej@erskinejones.com>
2025-03-22 18:57:21 +0000

    add pwa support

--- code/angular/src/app/app.config.ts
+++ code/angular/src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { SettingsHttpService } from './settings/settings.http.service';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export function initializeApp(settingsHttpService: SettingsHttpService) {
   return () => settingsHttpService.initializeApp();
@@ -19,6 +20,9 @@ export const appConfig: ApplicationConfig = {
       useFactory: initializeApp,
       deps: [SettingsHttpService],
       multi: true
-    }
+    }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 };
