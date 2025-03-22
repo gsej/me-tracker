8603df42d227313d2a5cc8dce01a354685e583bb
Gareth Erskine-Jones <gsej@erskinejones.com>
2025-03-22 18:33:20 +0000

    add and wireup settings

--- code/angular/src/app/app.component.ts
+++ code/angular/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SettingsService } from './settings/settings.service';
+import { PiComponent } from './components/pi/pi.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    PiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'me-tracker';
+
+  public gitHash: string = 'not set';
+
+  constructor(settingsService: SettingsService) {
+    this.gitHash = settingsService.settings.gitHash;
+  }
 }
